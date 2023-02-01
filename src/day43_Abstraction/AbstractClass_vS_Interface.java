f98a0aac241386320c0e0e05f5cce33b7f17172d
Mustafa <mustafamuezzinoglu@gmail.com>
2023-02-01 11:57:19 +0300

    day 43 added some that didnt add before

---
+++ src/day43_Abstraction/AbstractClass_vS_Interface.java
@@ -0,0 +1,28 @@
+package day43_Abstraction;
+
+
+interface A{
+int a = 100; //static and final
+// int v; // if it's not assigned it gives error
+
+}
+
+abstract class X{
+   public  int a;
+   private  int g;
+   protected  int f;
+    protected static int b;
+
+}
+
+
+
+
+public class AbstractClass_vS_Interface {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
